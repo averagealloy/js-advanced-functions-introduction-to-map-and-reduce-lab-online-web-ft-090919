a9d47da96e82c2f6ac5d2da7765ee7fe0f7f455f
averagealloy <m.mctighe10@gmail.com>
2020-01-06 21:25:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+
+
+function mapToNegativize(src) {
+    let a = []
+    for(let i = 0; i < src.length; i++){
+        a.push(-1*src[i])
+    }
+    return a 
+}
+
+
+function mapToNoChange(src) {
+    let a = []
+    for(let i = 0; i < src.length; i++){
+        a.push(src[i])
+    }
+    return a 
+}
+
+
+function mapToDouble(src) {
+    let a = []
+    for(let i = 0; i < src.length; i++){
+        a.push(2*src[i])
+    }
+    return a 
+}
+
+function mapToSquare(src) {
+    let a = []
+    for(let i = 0; i < src.length; i++){
+        a.push(src[i] * src[i])
+    }
+    return a 
+}
+
+function reduceToTotal(src, begin=0){
+    let total = begin 
+    for (let i = 0; i < src.length; i++){
+        total = total + src[i]
+    }
+    return total
+} 
+
+function reduceToAllTrue(src){
+    for (let i = 0; i < src.length; i++) {
+        if (!src[i]) return false
+    }
+    return true 
+}
+
+function reduceToAnyTrue(src){
+    for (let i = 0; i < src.length; i++) {
+        if (src[i]) return true
+    }
+    return false
+}
+
+
